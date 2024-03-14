cb007f5889af616be93de811498f0f9427121698
Vladislav Kabral <mr.kabral@mail.ru>
2024-03-14 22:25:04 +0300

    Add migration, model and service for ratings

---
+++ RatingService/src/main/java/by/modsen/taxiprovider/ratingservice/mapper/RatingMapper.java
@@ -0,0 +1,22 @@
+package by.modsen.taxiprovider.ratingservice.mapper;
+
+import by.modsen.taxiprovider.ratingservice.dto.rating.RatingDTO;
+import by.modsen.taxiprovider.ratingservice.model.Rating;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RatingMapper {
+
+    private final ModelMapper modelMapper;
+
+    public Rating toEntity(RatingDTO ratingDTO) {
+        return modelMapper.map(ratingDTO, Rating.class);
+    }
+
+    public RatingDTO toDTO(Rating rating) {
+        return modelMapper.map(rating, RatingDTO.class);
+    }
+}
